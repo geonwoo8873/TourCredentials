39ad1b4714feb4b4ff413504ed70b5876a71d033
geonwoo <geonwoo8873@outlook.kr>
2026-04-28 17:54:09 +0900

    Remove obsolete Docker, .NET project files, and scripts

--- Docker/PostgreSQL.Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM postgres:latest
-
-EXPOSE 9999:9999
-
-COPY ../PostgreSQL /usr/src/
-
-WORKDIR /usr/src/PostgreSQL
--- Scripts/PowerShell/Policy.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Warning: This script is for demonstration purposes only. It is not intended for production use and may contain security vulnerabilities. Use at your own risk."
-
-# Define a function to check if the current user has administrative privileges
-
-$CURRENT_USER = [Security.Principal.WindowsIdentity]::GetCurrent();
-
-Write-Host "Current user: $($CURRENT_USER.Name)"
